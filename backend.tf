54d684f91e0e3c354173d0ebdded32f22f87b956
Yuliia Bohosian <116050662+YulAwan@users.noreply.github.com>
2023-01-14 23:09:09 -0500

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">=0.12.0"
+  backend "s3" {
+    region  = "us-east-1"
+    profile = "default"
+    key     = "tfstatefile"
+    bucket  = "tfstatebucket3286"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "profile" {
+  type    = string
+  default = "default"
+}
+variable "region-master" {
+  type    = string
+  default = "us-east-1"
+}
+variable "region-worker" {
+  type    = string
+  default = "us-west-2"
+}
+variable "external_ip" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "workers-count" {
+  type    = number
+  default = 1
+}
+
+variable "instance-type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "webserver-port" {
+  type    = number
+  default = 8080
+}
+
+
+
+variable "dns-name" {
+  type    = string
+  default = "cmcloudlab0929.info."
+}
+
